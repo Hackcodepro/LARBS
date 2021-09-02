4c15a840977079ad207d11d349a961c0412a9ce3
Hackcodepro <90001877+Hackcodepro@users.noreply.github.com>
2021-09-03 00:08:04 +0500

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,8 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/Hackcodepro/Avoidrice.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/Hackcodepro/LARBS/main/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
